c221583af7d7b50aee99479a820bad32067c6410
Dipnoi <thomas.neele@gmail.com>
2011-02-21 14:57:04 +0100

    2 canvassen
    Signed-off-by: Dipnoi <thomas.neele@gmail.com>

--- game.js
+++ game.js
@@ -2,19 +2,33 @@ var intervalID;
 var rotation;
 var green;
 var canvas;
+var background;
 var ctx;
+var ctxB;
 
 function init() {
 	canvas = document.getElementById("canvas");
+	background = document.getElementById("background");
 	ctx = canvas.getContext("2d");
+	ctxB = background.getContext("2d");
 
 	ctx.fillStyle = "black";
+	
 	
 	drawTrans(ctx);
 	rotation = -10;
 	green = 0;
-	intervalID = window.setInterval("drawRot(ctx);",30);
-	drawRot(ctx);
+	intervalID = window.setInterval("drawRot(ctx);",1000/60);
+}
+
+function drawCircle(ctx, x, y){
+	ctx.beginPath();
+	ctx.arc(x, y, 4, 0, Math.PI*2, true);
+	ctx.closePath();
+	ctx.fill();
+}
+
+function clearAndDrawCircle(ctx, x, y) {
 }
 
 function drawTrans(ctx) {
@@ -32,7 +46,8 @@ function drawTrans(ctx) {
 function drawRot(ctx) {
 	ctx.save();
 	canvas.width = canvas.width;
-	drawTrans(ctx);
+	//drawTrans(ctx);
+	drawCircle(ctx,75,75);
 	ctx.globalCompositeOperation = "xor";
 	ctx.lineJoin = "round";
 	ctx.strokeStyle = 'rgb(0,' + green + ',0)';
